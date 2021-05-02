6d2314ded59913b64468c9aba1294c8c15747f3c
TeamBinaryBeast <binarybeastjr@gmail.com>
2021-05-02 20:50:37 +0600

    init -sway'

---
+++ CryptoWheelSpin/CryptoWheelSpin/routing.py
@@ -0,0 +1,11 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.wheelspin
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    )
+})
