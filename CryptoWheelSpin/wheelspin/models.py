30775e5e3f1c1465b64a489e3fea62b401f61dd6
TeamBinaryBeast <binarybeastjr@gmail.com>
2021-05-04 22:02:29 +0600

    models --credits

--- CryptoWheelSpin/wheelspin/models.py
+++ CryptoWheelSpin/wheelspin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Credits(models.Model):
+    user_f = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    def __str__(self):
+        return str(self.username)
+    objects = models.Manager()
+    class meta:
+        managed = True
+        db_table = 'Credits'
