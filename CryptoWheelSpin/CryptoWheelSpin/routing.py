61189cbf15eeffd5de069ad770e22acef40ec6f9
TeamBinaryBeast <binarybeastjr@gmail.com>
2021-05-04 21:54:29 +0600

    asgi

--- CryptoWheelSpin/CryptoWheelSpin/routing.py
+++ CryptoWheelSpin/CryptoWheelSpin/routing.py
@@ -1,11 +1,11 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-import chat.wheelspin
+import wheelspin.routing
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns
+            wheelspin.routing.websocket_urlpatterns
         )
     )
 })
